8fb847be072db61904153f43146901346007845c
Frank Denzer <fdenzer@gmail.com>
2014-11-25 16:30:37 +0100

    STABLE w/ cash, dummy option event trigger and dialog

--- models.py
+++ models.py
@@ -4,10 +4,11 @@ from django.db.models import DateField, CharField, TextField, IntegerField, Fore
 class TimeLineEvent(Model):
     group = CharField(max_length=30, default='derivative', blank=False)
     start = DateField(default='2014-01-01')
-    end = DateField(default='2015-01-01')
+    end = DateField(default='2014-01-03')
     title = CharField(max_length=100)
     description = TextField(blank=True)
     exchange_rate = FloatField(default=1.35, null=True)
+    url = URLField()
 
     def __unicode__(self):
         return self.title
